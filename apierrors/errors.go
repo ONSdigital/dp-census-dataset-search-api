3e6b0bdfd9f388e5e0cb17b002f78932d06f4f5d
nshumoogum <nathan.shumoogum@methods.co.uk>
2020-07-10 16:17:27 +0100

    Update swagger doc and fix response when topic doesn't exist

--- apierrors/errors.go
+++ apierrors/errors.go
@@ -10,10 +10,13 @@ var (
 	ErrMarshallingQuery       = errors.New("failed to marshal query to bytes for request body to send to elastic")
 	ErrParsingQueryParameters = errors.New("failed to parse query parameters, values must be an integer")
 	ErrTooManyTopicFilters    = errors.New("Too many topic filters, limited to a maximum of 10")
+	ErrTopicNotFound          = errors.New("Topic not found")
 	ErrUnmarshallingJSON      = errors.New("failed to parse json body")
 	ErrUnexpectedStatusCode   = errors.New("unexpected status code from elastic api")
 
-	NotFoundMap = map[error]bool{}
+	NotFoundMap = map[error]bool{
+		ErrTopicNotFound: true,
+	}
 
 	BadRequestMap = map[error]bool{
 		ErrEmptySearchTerm:        true,
